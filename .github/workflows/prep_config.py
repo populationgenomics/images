b511a3efb9dbd578986009ccac2cad3c495bce2d
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-11-24 23:40:05 +1100

    Fix

--- .github/workflows/prep_config.py
+++ .github/workflows/prep_config.py
@@ -8,5 +8,5 @@ import toml
 IMAGES_PREFIX = os.environ['IMAGES_PREFIX']
 
 d = toml.load('images.toml')
-d = {k: f'{IMAGES_PREFIX}/k:{v}' for k, v in d.items()}
+d = {k: f'{IMAGES_PREFIX}/{k}:{v}' for k, v in d.items()}
 print(toml.dumps({'images': d}))
