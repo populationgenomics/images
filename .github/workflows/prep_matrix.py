1c3c8b451799411d7308cdaa1ed015ec6b0c92f4
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-11-24 23:27:02 +1100

    Fix

--- .github/workflows/prep_matrix.py
+++ .github/workflows/prep_matrix.py
@@ -10,12 +10,17 @@ d = toml.load('images.toml')
 before_d = {}
 if Path(path := 'before/images.toml').exists():
     before_d = toml.load(path)
+
+# Changed only
 d = {
-    'include': [
-        {'name': name, 'tag': tag}
-        for name, tag in d.items()
-        if not before_d.get(name) or before_d[name] != tag
-    ]
+    name: tag
+    for name, tag in d.items()
+    if not before_d.get(name) or before_d[name] != tag
 }
+if d:
+    d = {'include': [{'name': name, 'tag': tag} for name, tag in d.items()]}
+else:
+    d = {}
+
 print(str(d).replace(' ', ''), end='', file=sys.stderr)
 print(str(d).replace(' ', ''), end='')
