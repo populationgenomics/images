0b371b8e7e32f7ce514caf0115371ef3c8895e17
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-12-13 14:40:51 +1100

    Lint

--- .github/workflows/prep_config.py
+++ .github/workflows/prep_config.py
@@ -9,8 +9,8 @@ import toml
 IMAGES_PREFIX = os.environ['IMAGES_PREFIX']
 
 d = toml.load('images.toml')
-# Manually adding cpg_workflows image, as we don't maintain it with 
-# images.toml, instead we check out the latest version from the 
+# Manually adding cpg_workflows image, as we don't maintain it with
+# images.toml, instead we check out the latest version from the
 # production-pipelines repository.
 d['cpg_workflows'] = sys.argv[1]
 d = {k: f'{IMAGES_PREFIX}/{k}:{v}' for k, v in d.items()}
