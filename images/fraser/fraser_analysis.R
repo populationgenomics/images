d574eaff4625d88ee3d27e19d3484d078a7ea553
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-02-02 15:54:09 +1100

    fixing the analysis script

--- images/fraser/fraser_analysis.R
+++ images/fraser/fraser_analysis.R
@@ -5,6 +5,7 @@ library(FRASER)
 library(tidyverse)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(org.Hs.eg.db)
+library(HDF5Array)
 library(ggplot2)
 
 parser <- ArgumentParser(description = "FRASER 2.0 Statistical Analysis")
@@ -17,73 +18,85 @@ parser$add_argument("--min_count", type = "integer", default = 5)
 parser$add_argument("--nthreads", type = "integer", default = 1)
 args <- parser$parse_args()
 
-# Force HDF5 for large matrix operations
-options("FRASER.maxSamplesNoHDF5" = 0)
-options("FRASER.maxJunctionsNoHDF5" = -1)
-
-z_cutoff <- if (is.null(args$z_cutoff)) NA else args$z_cutoff
+# --- 1. Parallelization Setup ---
+# Use the user-provided nthreads for all BiocParallel operations
 bp <- MulticoreParam(workers = args$nthreads)
 register(bp)
 
 # 1. Load the merged FDS
 # Python extracted the tar into args$fds_dir.
 # Inside is 'savedObjects/FRASER_{cohort_id}/...'
+options(delayedArray.block.size = 1e9) # 1GB blocks
 fds_name <- paste0("FRASER_", args$cohort_id)
-fds <- loadFraserDataSet(dir = args$fds_dir, name = fds_name)
+message(paste0("Loading Fraser Data Set: ", fds_name, " from ", args$fds_dir))
+fds <- loadFraserDataSet(dir = file.path(args$fds_dir, fds_name), name = fds_name)
 
-# 2. Filter Expression and Variability
-# This reduces the number of tests and improves power
-message("Filtering junctions...")
-fds <- filterExpressionAndVariability(fds, minDeltaPsi = 0.0, filter = TRUE)
 
-dir.create("plots/misc", recursive = TRUE, showWarnings = FALSE)
-png("plots/misc/filter_expression.png", width = 2000, height = 2000, res = 300)
-plotFilterExpression(fds, bins = 100)
+# --- 3. Filtering ---
+# It is critical to filter before fitting to reduce the size of the latent space matrices
+fds <- calculatePSIValues(fds, types = "jaccard", BPPARAM = bp)
+
+# Create QC directory
+dir.create("qc_plots", showWarnings = FALSE)
+
+# DOWNSAMPLING FOR PLOTS: Use 30,000 random junctions for QC to keep it fast
+set.seed(42)
+plot_idx <- sample(nrow(fds), min(nrow(fds), 30000))
+fds_plot_subset <- fds[plot_idx, ]
+
+message("Generating QC plots using downsampled subset...")
+png("qc_plots/filter_expression.png", width = 1200, height = 1200, res = 150)
+plotFilterExpression(fds_plot_subset, bins = 100)
 dev.off()
 
-# 3. Hyperparameter Optimization (bestQ) and Fitting
-# FRASER uses an autoencoder to control for latent confounders (batch effects)
-message("Finding optimal hyperparameter Q and fitting autoencoder...")
-# In FRASER 2.0, we typically fit psi5, psi3, and jaccard
-fds <- FRASER(fds, q = c(psi5=NA, psi3=NA, jaccard=NA), BPPARAM = bp)
+# Apply actual filtering to full dataset
+fds <- filterExpressionAndVariability(fds, minDeltaPsi = 0.0, filter = TRUE)
 
-# 4. Annotation
-message("Annotating results with UCSC hg38...")
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-orgDb <- org.Hs.eg.db
-fds <- annotateRangesWithTxDb(fds, txdb = txdb, orgDb = orgDb)
+# --- 4. Dimensionality Message ---
+raw_dim <- nrow(fds)
+fds_filtered <- fds[mcols(fds, type = "j")[, "passed"], ]
+filtered_dim <- nrow(fds_filtered)
 
-# 5. Extract Results
-message("Exporting results...")
-res <- results(fds,
+message(paste0("\n--- Filtering Summary ---"))
+message(paste0("Original junctions:   ", raw_dim))
+message(paste0("Filtered junctions:   ", filtered_dim))
+message(paste0("Reduction:            ", round((1 - (filtered_dim / raw_dim)) * 100, 2), "%"))
+
+# --- 5. Hyperparameter Optimization ---
+# Optimization must run on the filtered set
+opt_q <- bestQ(fds_filtered, type = "jaccard")
+
+png("qc_plots/best_q_optimization.png", width = 1200, height = 1200, res = 150)
+plotEncDimSearch(fds_filtered, type = "jaccard")
+dev.off()
+
+# --- 6. Fitting ---
+fds_fit <- FRASER(fds_filtered, q = opt_q, type = "jaccard", BPPARAM = bp)
+
+# QQ Plot (also uses a subset internally in FRASER, but we'll be explicit)
+png("qc_plots/qq_plot.png", width = 1200, height = 1200, res = 150)
+plotQQ(fds_fit, type = "jaccard")
+dev.off()
+
+# --- 7. Annotation ---
+fds_fit <- annotateRangesWithTxDb(fds_fit,
+                                  txdb = TxDb.Hsapiens.UCSC.hg38.knownGene,
+                                  orgDb = org.Hs.eg.db)
+
+# --- 8. Results & Compressed Export ---
+res <- results(fds_fit,
                padjCutoff = args$pval_cutoff,
                deltaPsiCutoff = args$delta_psi_cutoff,
-               zScoreCutoff = z_cutoff,
+               zScoreCutoff = args$z_cutoff,
                minCount = args$min_count)
 
-res_all <- results(fds, padjCutoff = 1, deltaPsiCutoff = 0, minCount = 0)
+# Extract all results for Jaccard
+res_all <- results(fds_fit, padjCutoff = 1, deltaPsiCutoff = 0, minCount = 0)
 
-write_csv(as.data.frame(res), "results.significant.csv")
-write_csv(as.data.frame(res_all), "results.all.csv")
+message("Saving results...")
+write_csv(as.data.frame(res), paste0(args$cohort_id, ".significant.csv"))
+write_csv(as.data.frame(res_all), paste0(args$cohort_id, ".all_results.csv.gz"))
 
-# 6. Generate Diagnostic Plots
-message("Generating Volcano and Q-Q plots...")
-dir.create("plots/volcano", recursive = TRUE)
-for(type in c("psi5", "psi3", "jaccard")){
-    png(paste0("plots/volcano/volcano_", type, ".png"), width = 2000, height = 2000, res = 300)
-    print(plotVolcano(fds, type = type, sampleID = sampleIDs(fds)[1])) # Plots first sample as example
-    dev.off()
-}
-
-# 7. Final Save
-# We save this in a generic folder so Python can easily grab it
-saveFraserDataSet(fds, dir = "output", name = paste0("Final_", args$cohort_id))
-
-# Summary statistics for the log
-sink("statistics_summary.txt")
-cat("Cohort ID:", args$cohort_id, "\n")
-cat("Total Samples:", length(sampleIDs(fds)), "\n")
-cat("Significant Events (adj P <", args$pval_cutoff, "):", nrow(res), "\n")
-sink()
-
-message("Analysis complete.")
+# --- 9. Final Save ---
+saveFraserDataSet(fds_fit, dir = getwd(), name = paste0(args$cohort_id, "_final"))
+message("Analysis Complete.")
