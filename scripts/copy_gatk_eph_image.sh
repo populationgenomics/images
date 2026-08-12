221a35e61dcadaf1ee7286c36f0585542e7731b9
Hope Tanudisastro <40783500+htanudisastro@users.noreply.github.com>
2026-08-13 09:28:18 +1000

    Create copy_gatk_eph_image.sh (#317)
    * Create copy_gatk_eph_image.sh
    
    * Update scripts/copy_gatk_eph_image.sh
    
    Co-authored-by: joshua m schmidt <joshua.schmidt@populationgenomics.org.au>
    
    ---------
    
    Co-authored-by: joshua m schmidt <joshua.schmidt@populationgenomics.org.au>

---
+++ scripts/copy_gatk_eph_image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+# Experimental GATK build for the FederateSVs tool, suggested by
+# Emma Pierce-Hoffman (Talkowski lab).
+# Upstream: https://github.com/broadinstitute/gatk
+#   branch eph_federate_svs @ 53b4f16a7fd07cc66b7560e6beaaa7fbe80d61ca
+# NOTE: the source lives in Broad's ephemeral (eph) registry and will be
+# deleted eventually. Rebuild from the commit above with a Dockerfile
+# before using this for any production callset.
+SOURCE_IMAGE="docker://us.gcr.io/broad-dsde-methods/eph/gatk:eph_federate_svs-53b4f16a7"
+IMAGE_NAME="gatk_federate_svs"
+IMAGE_TAG="53b4f16a7"
+
+gcloud auth configure-docker australia-southeast1-docker.pkg.dev
+skopeo copy ${SOURCE_IMAGE} docker://australia-southeast1-docker.pkg.dev/cpg-common/images/${IMAGE_NAME}:${IMAGE_TAG}
